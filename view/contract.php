3c448d739a129767e7205b6eed7c69d4a10dc319
Huynh-Nhu <122364274+Huynh-Nhu@users.noreply.github.com>
2023-04-16 22:59:46 +0700

    Add files via upload

---
+++ view/contract.php
@@ -0,0 +1,69 @@
+
+
+
+    <!-- CONNTRACT -->
+    <div class="row ">
+        <h1 class="text-bg-dark h-100" style="text-align: center;">CONTRACT</h1>   
+        <div class="col-lg-4 col-sm-3 " style="margin-top:50px; margin-left:10px">
+            <form class="border border-black " style="padding:20px; border-radius: 5px;">
+                <label  class="form-label">Name</label>
+                <input type="name" class="form-control"  placeholder="">
+                <label for="exampleFormControlInput2" class="form-label">Email</label>
+                 <input type="email" class="form-control" id="exampleFormControlInput2" placeholder="">
+                 <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <button class="btn btn-outline-dark mt-2">
+                    SEND
+                </button>
+            </form>
+         </div>
+         
+       
+        <div class="col-lg-4 col-sm-5" style="margin-top:50px;">
+            <p class="text-bg-dark" style="text-align: center; border-radius: 20px; font-family: 'Times New Roman', Times, serif;  font-size: 30px;" >
+                Liên Hệ
+                <ul style="text-align:justify; border-radius: 20px; font-family: 'Times New Roman', Times, serif;  font-size: 20px;" >
+                    <li>Sản phẩm bị lỗi</li>
+                    <li>Giao nhầm hàng hay nhầm màu</li>
+                    <li>Quá ngày hẹn giao sản phẩm nhưng chưa nhận được</li>
+                    <li>Trong vòng 2h sau khi đặt sản phẩm muốn hủy.Sau 2h sẽ không chấp nhận hủy đơn đã đặt</li>
+
+                </ul>
+               
+            </p>
+            
+            <p class="">
+                <p><i class="fa fa-map-marker-alt" style="width: 20px; text-align: center;"></i> Địa chỉ:Đại học Cần Thơ,Đ.3/2,P.Xuân Khánh,Q.Ninh Kiều,TP.Cần Thơ</p>
+                <p><i class="fa fa-phone-alt "style="width: 20px; text-align: center;"></i> SDT-1:+915107415</p>
+                <p><i class="fa fa-phone-alt "style="width: 20px; text-align: center;"></i> SDT-2:+846316361</p>
+            </p>
+            
+        </div>
+        <div class="col-lg-3 col-sm-3 d-flex mt-5" style="">
+           
+        <img class="w-100 h-100" src="../img/logo/con-1.png" alt="" >
+        </div>
+    </div>   
+    <div>
+    <div class="d-flex " style="justify-content: center;margin-top:50px;">
+                <a href="https://www.facebook.com/" class="text-bg-dark" style="margin: 5px; padding: 20px;  border-radius: 150px;">
+                    <i class="fab fa-twitter" style="width: 20px; text-align: center;"></i>
+
+                </a>
+                <a href="https://www.instagram.com/" class="text-bg-dark" style="margin: 5px; padding: 20px;  border-radius: 100px;">
+                    <i class="fab fa-facebook-f"  style="width: 20px; text-align: center;"></i>
+
+                </a>
+                <a href="https://twitter.com/" class="text-bg-dark" style="margin: 5px; padding: 20px;  border-radius: 150px;">
+                    <i class="fab fa-instagram" style="width: 20px; text-align: center;"></i>
+
+                </a>
+                <a href="https://www.google.com/maps/place/%C4%90%E1%BA%A1i+h%E1%BB%8Dc+C%E1%BA%A7n+Th%C6%A1/@10.029939,105.7684266,17z/data=!3m1!4b1!4m6!3m5!1s0x31a0895a51d60719:0x9d76b0035f6d53d0!8m2!3d10.0299337!4d105.7706153!16s%2Fm%2F02r6wmy" class="text-bg-dark" style="margin: 5px; padding: 20px;  border-radius: 150px;">
+                    <i class="fa fa-map-marker-alt" style="width: 20px; text-align: center;"></i>
+
+                </a>
+
+            </div>
+    </div>
+ </body>
+</html>
