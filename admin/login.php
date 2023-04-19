f5236f092a22a72db3bc9d89dac5a00b4325e67a
B2014580_DangThanhLiem <122429919+DangThanhLiem@users.noreply.github.com>
2023-04-19 13:48:19 +0700

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -1,18 +1,20 @@
 <?php
 session_start();
 ob_start();
-include "./model/connectdb.php";
-include "./model/user.php";
+include "../model/connectdb.php";
+include "../model/user.php";
 if (isset($_POST['dangnhap']) && $_POST['dangnhap']) {
     $user = $_POST['user'];
     $pass = $_POST['pass'];
     $role = checkuser($user, $pass);
     $_SESSION['role'] = $role;
+    // nếu là quyền admin thì nhảy qua trang chủ còn không thì báo lỗi
     if ($role == 1)
         header('location: index.php');
-    else {
-        $txt_error = "Username hoặc Password không tồn tại!";
-        // header('location: login.php');
+    else if ($role == 0) {
+        $txt_error = "Bạn không phải admin!";
+    } else {
+        $txt_error = "Username hoặc password không tồn tại!";
     }
 }
 ?>
@@ -30,10 +32,11 @@ if (isset($_POST['dangnhap']) && $_POST['dangnhap']) {
 <body>
     <div class="main">
         <h2>Login</h2>
+        <!-- kiểm tra diều kiện trong chính cái trang này -->
         <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-            <input type="text" name="user" value="">
+            <input type="text" name="user" value="" placeholder="Nhập tài khoản">
             <div class="container">
-                <input type="password" name="pass" placeholder="Nhập Mật Khẩu" required>
+                <input type="password" name="pass" placeholder="Nhập mật khẩu" required>
             </div>
             <input type="submit" name="dangnhap" value="Đăng nhập">
             <?php
